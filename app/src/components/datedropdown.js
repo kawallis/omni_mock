5354fc6034c9cc20f003f09e0ba69c10f13456e9
Chris_Wallace <kawallis@icloud.com>
2017-07-13 15:20:33 -0700

    added time formatting to box

--- app/src/components/datedropdown.js
+++ app/src/components/datedropdown.js
@@ -3,19 +3,21 @@ import './datedropdown.css';
 import arrow from '../assets/arrow.svg';
 
 class DateDropDown extends Component {
-
   render () {
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
     let slots = null;
-    console.log(this.props.dates);
     if ( this.props.slotsView ) {
-      slots = <div className="SlotsBox">
-              { this.props.dates.map((time, i) => {
-                return (
-    
-                    <p key={i}>{time.timeslot}</p>
-
-                );
-              })}
+      slots = <div className="SlotsBox">   
+                <ul>
+                  {this.props.dates.map((time, i) => {
+                    return <li className="Times" key={i}>{`${time.timestamp.getHours()} : ${addZero(time.timestamp.getMinutes())}`}<span className="Prices">{time.price}</span></li>;
+                  })}
+                </ul>  
               </div>
     }else {
       slots = null;
