8bbb4c97e9d7b8322265cc3fa3b66e5028a14594
Chris_Wallace <kawallis@icloud.com>
2017-07-13 14:51:18 -0700

    got days working

--- app/src/DateSelector/DateTimeBox.js
+++ app/src/DateSelector/DateTimeBox.js
@@ -22,7 +22,10 @@ class DateTimeBox extends Component {
     fetch('/api/timeslots', settings)
     .then(res => res.json())
     .then(res => {
-      addDates(res);
+      let arr = res.map(item => {
+        return {timestamp: new Date(item.timestamp*1000), price: item.price}
+      })
+      addDates(arr);
     });
   }
 
@@ -33,17 +36,26 @@ class DateTimeBox extends Component {
   }
 
   findPrices(date) {
-    const { addDates } = this.props;
-    let settings = {
-      method: 'GET',
-      headers: new Headers({ 'Content-Type': 'application/json' })
-    };
-    fetch(`/api/timeslots/${date}`, settings)
-    .then(res => res.json())
-    .then(res => {
-      changeDates(res);
-    //TODO
+    const { availableTimes } = this.props;
+    Date.prototype.addDays = function(days) {
+      var dat = new Date(this.valueOf());
+      dat.setDate(dat.getDate() + days);
+      return dat;
+    }
+    let arr = availableTimes.filter(item => {
+      return (item.timestamp >= date && item.timestamp <= date.addDays(1)); 
     });
+    console.log(arr);
+    // let settings = {
+    //   method: 'GET',
+    //   headers: new Headers({ 'Content-Type': 'application/json' })
+    // };
+    // fetch(`/api/timeslots/${date}`, settings)
+    // .then(res => res.json())
+    // .then(res => {
+    //   changeDates(res);
+    // //TODO
+    // });
   }
 
   render() {
